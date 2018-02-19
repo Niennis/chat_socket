deb8f70d2f013429459bdfa0f7826c048d413a65
Niennis <estefania.osses.v@gmail>
2018-02-19 08:02:41 -0300

    Corrección indentación

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,58 +2,58 @@ let numUsers = 0;
 let yourName = 'You';
 let loggedIn = false;
 
-$(function () {
-    var socket = io('http://ec2-34-227-11-223.compute-1.amazonaws.com:3000');
+$(function() {
+  var socket = io('http://ec2-34-227-11-223.compute-1.amazonaws.com:3000');
 
-    $('form').submit(function (e) {
-        e.preventDefault();
-        const msg = $('#m').val();
-        addToChat(`<li><span class="you">${yourName}</span>: ${msg} </li>`);
-        socket.emit('new message', msg);
-        $('#m').val('');
-    });
+$('form').submit(function(e) {
+    e.preventDefault();
+    const msg = $('#m').val();
+    addToChat(`<li><span class="you">${yourName}</span>: ${msg} </li>`);
+    socket.emit('new message', msg);
+    $('#m').val('');
+  });
 
-    socket.on('new message', function (obj) {
-        const name = obj.username ? obj.username : 'Guest';
-        addToChat(`<li><span class="ppl">${name}</span>: ${obj.message} </li>`);
-    });
+  socket.on('new message', function(obj) {
+    const name = obj.username ? obj.username : 'Guest';
+    addToChat(`<li><span class="ppl">${name}</span>: ${obj.message} </li>`);
+  });
 
-    socket.on('chat message', function (msg) {
-        addToChat(`<li><span class="ppl">Guest</span>: ${msg} </li>`);
-    });
+  socket.on('chat message', function(msg) {
+    addToChat(`<li><span class="ppl">Guest</span>: ${msg} </li>`);
+  });
 
-    $('#btn').click(function (e) {
-        e.preventDefault();
-        if ($('#userid').val().length > 0 && !loggedIn) {
-            yourName = $('#userid').val();
-            socket.emit('add user', yourName);
-            $('#chat').removeClass('d-none');
-            $('#firstLogin').addClass('d-none');
-        }
-    });
-    $('#guest').click(function (e) {
-        e.preventDefault();
-        $('#chat').removeClass('d-none');
-        $('#firstLogin').addClass('d-none');
-    });
+  $('#btn').click(function(e) {
+    e.preventDefault();
+    if ($('#userid').val().length > 0 && !loggedIn) {
+      yourName = $('#userid').val();
+      socket.emit('add user', yourName);
+      $('#chat').removeClass('d-none');
+      $('#firstLogin').addClass('d-none');
+    }
+  });
+  $('#guest').click(function(e) {
+    e.preventDefault();
+    $('#chat').removeClass('d-none');
+    $('#firstLogin').addClass('d-none');
+  });
 
-    socket.on('login', function (obj) {
-        addToChat(`<li>you logged in as <span class="you">${yourName}</span></li>`);
-        loggedIn = true;
-    });
+  socket.on('login', function(obj) {
+    addToChat(`<li>you logged in as <span class="you">${yourName}</span></li>`);
+    loggedIn = true;
+  });
 
-    socket.on('user joined', function (obj) {
-        addToChat(`<li><span class="ppl">${obj.username}</span> logged in</li>`);
-        numUsers = obj.numUsers;
-    });
+  socket.on('user joined', function(obj) {
+    addToChat(`<li><span class="ppl">${obj.username}</span> logged in</li>`);
+    numUsers = obj.numUsers;
+  });
 
-    socket.on('user left', function (obj) {
-        addToChat(`<li><span class="ppl">${obj.username}</span> logged out</li>`);
-        numUsers = obj.numUsers;
-    });    
+  socket.on('user left', function(obj) {
+    addToChat(`<li><span class="ppl">${obj.username}</span> logged out</li>`);
+    numUsers = obj.numUsers;
+  });    
 });
 
 function addToChat(msg) {
-    $('#messages').append(msg);
-    window.scrollTo(0, document.body.scrollHeight);
+  $('#messages').append(msg);
+  window.scrollTo(0, document.body.scrollHeight);
 }
--- index.js
+++ index.js
@@ -5,21 +5,20 @@ var path = require('path');
 var express = require('express');
 var io = require('socket.io')(http);
 
-app.get('/', function(req, res){
+app.get('/', function(req, res) {
   res.sendFile(__dirname + '/index.html');
 });
 
 app.use(express.static('assets'));
 
-io.on('connection', function(socket){
+io.on('connection', function(socket) {
   console.log('connected');  
-  socket.on('chat message', function(msg){
+  socket.on('chat message', function(msg) {
     io.emit('chat message', msg);
     console.log('mensaje');
-    
   });
 });
 
-http.listen(port, function(){
+http.listen(port, function() {
   console.log('listening on *:' + port);
 });
